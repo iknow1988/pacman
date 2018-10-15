e27b86963149406f330c1083d5c3d31f46cb1e35
Kazi Abir Adnan <kadnan@student.unimelb.edu.au>
2018-10-15 19:43:36 +1100

    autorun

--- pacman-contest/autorun.py
+++ pacman-contest/autorun.py
@@ -3,8 +3,8 @@ from time import sleep
 from os import listdir
 
 i = 0
-n = 10
-folder = "C:\Users\Kazi Abir Adnan\Google Drive (kadnan@student.unimelb.edu.au)\Unimelb Courses\Semester 2\COMP90054\Assignments\Project 2\pacman-contest\layouts2"
+n = 2
+folder = "C:\Users\Kazi Abir Adnan\Google Drive (kadnan@student.unimelb.edu.au)\Unimelb Courses\Semester 2\COMP90054\Assignments\Project 2\pacman-contest\layouts"
 
 l=os.listdir(folder)
 li=[x.split('.')[0] for x in l]
@@ -14,8 +14,7 @@ for layout in li:
     print "\n", layout, " . We will play for ", str(n), " times"
     sleep(2)
     fileName = r'"C:\Users\Kazi Abir Adnan\Google Drive (kadnan@student.unimelb.edu.au)\Unimelb Courses\Semester 2\COMP90054\Assignments\Project 2\pacman-contest\capture.py"'
-<<<<<<< Updated upstream
-    args = " -b myTeam-Kazi -r myTeam-V1 -l " + layout + " -z 0.5"
+    args = " -r myTeam-Final -b myTeam -l " + layout + " -z 0.5"
     args = args +" -q -n "+ str(n)
     os.system("python " + fileName + args)
     print "LEFT : ", str(len(li) - (i + 1))
@@ -29,10 +28,7 @@ for layout in li:
     print "\n", layout, " . We will play for ", str(n), " times"
     sleep(2)
     fileName = r'"C:\Users\Kazi Abir Adnan\Google Drive (kadnan@student.unimelb.edu.au)\Unimelb Courses\Semester 2\COMP90054\Assignments\Project 2\pacman-contest\capture.py"'
-    args = " -r myTeam-Kazi -b myTeam-V1 -l " + layout + " -z 0.5"
-=======
-    args = " -r myTeam-Kazi -b myTeam -l " + layout + " -z 0.5"
->>>>>>> Stashed changes
+    args = " -r myTeam -b myTeam-Final -l " + layout + " -z 0.5"
     args = args +" -q -n "+ str(n)
     os.system("python " + fileName + args)
     print "LEFT : ", str(len(li) - (i + 1))
